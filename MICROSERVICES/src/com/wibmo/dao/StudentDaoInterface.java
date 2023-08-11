1f4ff9b8cd571e04182391a6d911b7f1ac411d71
Himank Jain <himank.jain@wibmo.com>
2023-08-11 15:59:50 +0530

    adding registraionDaoInterface

--- MICROSERVICES/src/com/wibmo/dao/StudentDaoInterface.java
+++ MICROSERVICES/src/com/wibmo/dao/StudentDaoInterface.java
@@ -9,6 +9,27 @@ package com.wibmo.dao;
  */
 public interface StudentDaoInterface {
 	
-	public String addStudent(Student student);
-	public String getStudentId(Student userId);
+	
+	/**
+	 * Method to add student to database
+	 * @param student: student object containing all the fields
+	 * @return true if student is added, else false
+	 * @throws StudentNotRegisteredException
+	 */
+	public String addStudent(Student student) throws StudentNotRegisteredException;
+	
+	
+	/**
+	 * Method to retrieve Student Id from User Id
+	 * @param userId
+	 * @return Student Id
+	 */
+	public String getStudentId(String userId);
+	
+	/**
+	 * Method to check if Student is approved
+	 * @param studentId
+	 * @return boolean indicating if student is approved
+	 */
+	public boolean isApproved(String studentId);
 }

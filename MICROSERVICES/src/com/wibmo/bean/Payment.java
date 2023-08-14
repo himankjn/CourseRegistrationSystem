096dff0091f0efaa743ffb3a13c046199e01aa0c
bhuvanWibmo <terli.bc@wibmo.com>
2023-08-14 10:46:05 +0530

    update bean add Payment and Notification

---
+++ MICROSERVICES/src/com/wibmo/bean/Payment.java
@@ -0,0 +1,56 @@
+package com.wibmo.bean;
+
+public class Payment {
+	private String studentId;
+	/**
+	 * @return the studentId
+	 */
+	public String getStudentId() {
+		return studentId;
+	}
+	/**
+	 * @param studentId the studentId to set
+	 */
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+	/**
+	 * @return the invoiceId
+	 */
+	public String getInvoiceId() {
+		return invoiceId;
+	}
+	/**
+	 * @param invoiceId the invoiceId to set
+	 */
+	public void setInvoiceId(String invoiceId) {
+		this.invoiceId = invoiceId;
+	}
+	/**
+	 * @return the amount
+	 */
+	public double getAmount() {
+		return amount;
+	}
+	/**
+	 * @param amount the amount to set
+	 */
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	/**
+	 * @return the status
+	 */
+	public boolean isStatus() {
+		return status;
+	}
+	/**
+	 * @param status the status to set
+	 */
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	private String invoiceId;
+	private double amount;
+	private boolean status;
+}

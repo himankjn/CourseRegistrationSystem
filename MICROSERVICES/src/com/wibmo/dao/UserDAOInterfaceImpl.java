2d970cfb818f131d340a659b254486b17ada5f82
bhuvanWibmo <terli.bc@wibmo.com>
2023-08-16 14:08:07 +0530

    update userInterfaceDAO

---
+++ MICROSERVICES/src/com/wibmo/dao/UserDAOInterfaceImpl.java
@@ -0,0 +1,103 @@
+package com.wibmo.dao;
+
+import java.sql.*;
+import com.wibmo.constants.*;
+import com.wibmo.utils.*;
+
+/**
+ * @author Bhuvan
+ */
+
+/**
+ * UserDAOInterface implementation 
+ */
+public class UserDAOInterfaceImpl implements UserDAOInterface{
+	PreparedStatement stmt = null;
+	private static final Connection conn = DBUtils.getConnection();
+	private static volatile UserDAOInterfaceImpl instance = null;
+	
+	/**
+	 * Default constructor
+	 */
+	private UserDAOInterfaceImpl() {
+		
+	}
+	/**
+	 * Method to make UserDAOInterfaceImpl Singleton
+	 * @return
+	 */
+	public static UserDAOInterfaceImpl getInstance() {
+		if(instance==null)
+		{
+			synchronized(UserDAOInterfaceImpl.class){
+				instance=new UserDAOInterfaceImpl();
+			}
+		}
+		return instance;
+	}
+	
+	/**
+	 * method to verify credentials
+	 * @param userId, password to verify user
+	 * @return boolean
+	 */
+	@Override
+	public boolean verifyCredentials(String userId, String password) {
+		int status = 1;
+		try {
+			stmt = conn.prepareStatement(SQLQueriesConstant.GET_USER);
+			stmt.setString(1, userId);
+			stmt.setString(2, password);
+			status = stmt.executeUpdate();
+		}catch(SQLException se)
+		{
+			se.printStackTrace();
+		}
+		return status>0;
+	}
+	
+	/**
+	 * method to update password of user with Id userId
+	 * @param userId, newPassword 
+	 * @return update Status
+	 */
+	@Override
+	public boolean updatePassword(String userId, String newPassword) {
+		int status = 1;
+		try {
+			stmt = conn.prepareStatement(SQLQueriesConstant.UPDATE_USER);
+			stmt.setString(1, newPassword);
+			stmt.setString(2, userId);
+			status = stmt.executeUpdate();
+		}catch(SQLException se)
+		{
+			se.printStackTrace();
+		}
+		return status>0;
+	}
+	
+	/**
+	 * method to get role of user
+	 * @param userId 
+	 * @return Role of user
+	 */
+	@Override
+	public String getRole(String userId) {
+		String role = null;
+		try {
+			stmt = conn.prepareStatement(SQLQueriesConstant.GET_ROLE);
+			stmt.setString(1, userId);
+			ResultSet rs = stmt.executeQuery(SQLQueriesConstant.GET_ROLE);
+			if(rs.next())
+			{
+				role = rs.getString("role");
+			}
+			
+		}catch(SQLException se)
+		{
+			se.printStackTrace();
+		}
+		return role;
+	}
+
+}

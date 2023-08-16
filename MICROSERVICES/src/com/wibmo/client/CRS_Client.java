ea30a53f5eff41e7a1b28a9ca15dbf22303a04ed
Himank Jain <himank369123@gmail.com>
2023-08-16 10:55:29 +0530

    new changes

--- MICROSERVICES/src/com/wibmo/client/CRS_Client.java
+++ MICROSERVICES/src/com/wibmo/client/CRS_Client.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 /**
  * 
  */
-public class CRS_MainClient {
+public class CRS_Client {
 
 	/**
 	 * @param args
@@ -18,14 +18,14 @@ public class CRS_MainClient {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		LocalDateTime date= LocalDateTime.now();
-		System.out.println("====================Welcome To CRS System!====================");
-		System.out.println("================"+date.toLocalTime()+" "+date.toLocalDate()+"================");
+		System.out.println("===================Weclome To CRS Application!====================");
+		System.out.println("=============="+date.toLocalTime() + " " +date.toLocalDate()+"===============");
 		
 		System.out.println("Enter you choice:");
 		System.out.println("1. Student Registration\n2. Login\n3. Update Password\n4. Exit");
-		Scanner sc= new Scanner(System.in);
-		int ch=sc.nextInt();
-		switch(ch) {
+		Scanner sc = new Scanner(System.in);
+		int choice=sc.nextInt();
+		switch(choice) {
 		case 1: System.out.println("Student Registration Menu !");
 				break;
 		case 2: System.out.println("Login Success Assumed\nProf. Login Menu:");
@@ -37,7 +37,7 @@ public class CRS_MainClient {
 		case 3: System.out.println("Update Passowd Menu");
 		 		break;
 		case 4: System.out.println("Exit!");
-				System.exit(ch);
+				System.exit(choice);
 		}
 	}
 }
